b4f26b3446f868ed602bae6c816602c0e51dae13
Gabriel-Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2026-06-13 16:04:54 -0400

    Update site configuration for new user details

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Gabriel Xu",
+  title: "Computer Science Student at Carnegie Mellon University",
+  description: "Portfolio website of Gabriel Xu",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "gabrielx8421@gmail.com",
+    linkedin: "https://linkedin.com/in/xu-gabriel",
+    github: "https://github.com/Gabriel-Xu",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
